71ceedaf93a5bb398a1f0f1d41f79151280b25bb
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-19 19:31:53 -0500

    first deploy
    Getting 4 cards for each player. Going to need to do a lot of destructoring.

--- index.js
+++ index.js
@@ -10,14 +10,50 @@ fetch(url)
   .catch((err) => {
     console.log(`error ${err}`);
   });
-document.querySelector("button").addEventListener("click", drawTwo);
+document.querySelector("button").addEventListener("click", drawFirstPlayer);
 
-function drawTwo() {
+function drawFirstPlayer() {
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
-      document.querySelector(".player1").src = data.cards[0].image;
+      //   document.querySelector(".player1").src = data.cards[0].image;
+
+      document.querySelector(".card1").src = data.cards[0].image;
+      document.querySelector(".card2").src = data.cards[1].image;
+      document.querySelector(".card3").src = data.cards[2].image;
+      document.querySelector(".card4").src = data.cards[3].image;
+
+      document.querySelector(".player2").src = data.cards[1].image;
+
+      let player1Value = convertToNum(data.cards[0].value);
+      let player2Value = convertToNum(data.cards[1].value);
+
+      if (player1Value > player2Value) {
+        document.querySelector("h3").innerText = "Player 1 Wins";
+      } else if (player1Value < player2Value) {
+        document.querySelector("h3").innerText = "Player 2 Wins";
+      } else {
+        document.querySelector("h3").innerText = "Time for War!";
+      }
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
+
+function drawSecondPlayer() {
+  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      //   document.querySelector(".player1").src = data.cards[0].image;
+
+      document.querySelector(".card1").src = data.cards[0].image;
+      document.querySelector(".card2").src = data.cards[1].image;
+      document.querySelector(".card3").src = data.cards[2].image;
+      document.querySelector(".card4").src = data.cards[3].image;
+
       document.querySelector(".player2").src = data.cards[1].image;
 
       let player1Value = convertToNum(data.cards[0].value);
