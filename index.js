d0ec48dac7a69f996526269c9c8a4d294c2a6dbd
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-05-02 18:21:41 -0500

    made logic for game

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let deckId = "";
 
+let player1Cards = [];
+
 const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
 fetch(url)
   .then((res) => res.json()) // parse response as JSON
@@ -10,15 +12,36 @@ fetch(url)
   .catch((err) => {
     console.log(`error ${err}`);
   });
-document.querySelector("button").addEventListener("click", drawFirstPlayer);
+// document.querySelector("button").addEventListener("click", drawFirstPlayer);
 
 document.querySelector(".p1-card1").addEventListener("click", function () {
-  prompt("Is card red or black?");
+  //   prompt("Is card red or black?");
 
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
+      const chosenColor = document.querySelector(".color").value;
       console.log(data);
+      if (
+        (data.cards[0].suit === "HEARTS" ||
+          data.cards[0].suit === "DIAMONDS") &&
+        chosenColor === "Red"
+      ) {
+        document.querySelector(".color-result").innerText =
+          "You won by choosing red! Select a player to take 2 drinks!";
+      } else if (
+        (data.cards[0].suit === "SPADES" || data.cards[0].suit === "CLUBS") &&
+        chosenColor === "Black"
+      ) {
+        document.querySelector(".color-result").innerText =
+          "You won by choosing black! Select a player to take 2 drinks!";
+      } else {
+        document.querySelector(".color-result").innerText = "YOU LOST! Drink 2";
+        // console.log("!");
+      }
+      player1Cards.push(convertToNum(data.cards[0].value));
+      console.log(player1Cards);
+      console.log(chosenColor);
       //   document.querySelector(".player1").src = data.cards[0].image;
 
       document.querySelector(".p1-card1").src = data.cards[0].image;
@@ -33,6 +56,30 @@ document.querySelector(".p1-card2").addEventListener("click", function () {
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
+      const chosenHigher = document.querySelector(".higher").value;
+
+      if (
+        player1Cards[0] < convertToNum(data.cards[0].value) &&
+        chosenHigher === "Higher"
+      ) {
+        document.querySelector(".higher-result").innerText =
+          "You won by chosing higher! Select a player to take 4 drinks!";
+        // console.log("You won by chosing higher!");
+      } else if (
+        player1Cards[0] > convertToNum(data.cards[0].value) &&
+        chosenHigher === "Lower"
+      ) {
+        document.querySelector(".higher-result").innerText =
+          "You won by chosing lower! Select a player to take 4 drinks!";
+        // console.log("You won by chosing lower!");
+      } else {
+        document.querySelector(".higher-result").innerText =
+          "YOU LOST! Drink 4!";
+        // console.log("YOU LOST! Drink 4!");
+      }
+
+      player1Cards.push(convertToNum(data.cards[0].value));
+      console.log(player1Cards);
       //   document.querySelector(".player1").src = data.cards[0].image;
 
       document.querySelector(".p1-card2").src = data.cards[0].image;
@@ -47,6 +94,36 @@ document.querySelector(".p1-card3").addEventListener("click", function () {
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
+      const chosenInOut = document.querySelector(".in-out").value;
+      console.log(chosenInOut);
+      if (
+        (player1Cards[0] < convertToNum(data.cards[0].value) &&
+          player1Cards[1] > convertToNum(data.cards[0].value)) ||
+        (player1Cards[1] < convertToNum(data.cards[0].value) &&
+          player1Cards[0] > convertToNum(data.cards[0].value) &&
+          chosenInOut === "In")
+      ) {
+        document.querySelector(".in-result").innerText =
+          "You won by chosing in! Select a player to take 6 drinks!";
+        // console.log("You won by chosing higher!");
+      } else if (
+        (player1Cards[0] > convertToNum(data.cards[0].value) &&
+          player1Cards[1] < convertToNum(data.cards[0].value)) ||
+        (player1Cards[1] > convertToNum(data.cards[0].value) &&
+          player1Cards[0] < convertToNum(data.cards[0].value) &&
+          chosenInOut === "Out")
+      ) {
+        document.querySelector(".in-result").innerText =
+          "You won by chosing out! Select a player to take 6 drinks!";
+        // console.log("You won by chosing lower!");
+      } else {
+        document.querySelector(".in-result").innerText = "YOU LOST! Drink 6!";
+        // console.log("YOU LOST! Drink 4!");
+      }
+
+      player1Cards.push(convertToNum(data.cards[0].value));
+      console.log(player1Cards);
+
       //   document.querySelector(".player1").src = data.cards[0].image;
 
       document.querySelector(".p1-card3").src = data.cards[0].image;
@@ -61,6 +138,17 @@ document.querySelector(".p1-card4").addEventListener("click", function () {
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
+      let chosenSuit = document.querySelector(".suit").value;
+
+      if (chosenSuit === data.cards[0].suit) {
+        document.querySelector(".suit-result").innerText =
+          "You won by chosing the correct suit! Select a player to take 8 drinks!";
+      } else {
+        document.querySelector(".suit-result").innerText = "YOU LOST! Drink 8!";
+      }
+
+      player1Cards.push(convertToNum(data.cards[0].value));
+      console.log(player1Cards);
       //   document.querySelector(".player1").src = data.cards[0].image;
 
       document.querySelector(".p1-card4").src = data.cards[0].image;
